d04780ed1d30034f2a4134b9cf073e58acfdc493
flashfan88 <lavarjohnson20@gmail.com>
2019-08-08 12:31:08 -0500

    graded work

---
+++ index3.js
@@ -0,0 +1,42 @@
+//Excercise 1
+//Use a for loop to print numbers -3 through 8.
+
+//for(var i = -3; i<=8; i=i+1){
+
+//    console.log(i);
+//}
+
+//Exercise 2:
+//Use a for loop to print from 0 to 50. Once it gets to 10,
+// print "TEN!!!" instead of the number.
+
+// for(var i = 0; i<=50; i=i+1) {
+//     console.log(i);
+//     if (i === 10)
+//     {
+//
+//         console.log("TEN!");
+//
+//     }
+//
+// }
+
+
+//Exercise 3
+//Prompt the user to enter a number. Prompt the user to enter another number.
+//Create a for loop that counts from the first number to the second number.
+
+var userInput = prompt("Enter a Number");
+var userInput2 = prompt("Enter another Number");
+
+for(var i = 0; i<=100000; i=i+1){
+
+    if(userInput + userInput2){
+
+    }
+
+}
+
+
+
+
